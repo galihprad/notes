e2c20f26052f943d4919cc152575f4f777b8ec17
Galih Pradipto Wisnujati <galih.p.w.j@gmail.com>
2019-11-15 09:18:18 +0700

    fix firebase functionality (#6)

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,102 +1,43 @@
-import React, { useState, useEffect } from "react";
-import Card from "./Card";
-import firebase from "../config/fbConfig";
+import React, { useState } from "react";
 const uuidv4 = require("uuid/v4");
 
-const styleInput = {
-  display: "flex",
-  justifyContent: "center"
-};
-const styleListCard = {
-  justifyContent: "center",
-  flexWrap: "wrap"
-};
-
-const db = firebase.firestore().collection("notes1");
-let titles = "";
-let contents = "";
-
-const Input = () => {
-
-  const [List, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [refetch, setRefetch] = useState(true);
-
-  useEffect(() => {
-    console.log("FETCH");
-    const ListDB = [];
-    async function result() {
-      await db.get().then(snapshot => {
-        snapshot.docs.forEach(item => {
-          ListDB.push({
-            title: item.data().title,
-            content: item.data().content,
-            id: item.id
-          });
-        });
-      });
-      console.log("listdb", ListDB);
-      setIsLoading(false);
-      setList(ListDB);
-    }
-    result();
-  }, [refetch]);
-
-  const RefetchData = () => setRefetch(!refetch);
-
-  const handleChangeTitle = e => {
-    titles = e.target.value;
-  };
-
-  const handleChangeContent = e => {
-    contents = e.target.value;
-  };
+const Input = props => {
+  const { List, setList, db } = props;
+  const [titleVals, setTitleVals] = useState("");
+  const [contentVals, setContentVals] = useState("");
 
   const handleAddData = e => {
     e.preventDefault();
+    if (titleVals === "" && contentVals === "") {
+      return;
+    }
     const ids = uuidv4();
-    let addData = [{ id: ids, title: titles, content: contents }];
-    setList(addData.concat(List));
-    db.doc(ids).set({ title: titles, content: contents });
+
+    // add locally
+    let newData = [{ id: ids, title: titleVals, content: contentVals }];
+    setList(newData.concat(List));
+    setTitleVals("");
+    setContentVals("");
+
+    // add to DB
+    db.doc(ids).set({ title: titleVals, content: contentVals });
   };
-
-
-  const handleDelete = id => {
-    console.log("delete");
-    let newList = List.filter(item => item.id !== id);
-    setList(newList);
-    db.doc(id).delete();
-  };
-
-
-  const ListCard = isLoading
-    ? "tunggu dilit..."
-    : List.map(item => (
-        <>
-          <Card
-            db={db}
-            handleDelete={handleDelete}
-            RefetchData={RefetchData}
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            content={item.content}
-          />
-        </>
-      ));
-
-  console.log("RENDER", List);
-
+  console.log("RENDER INPUT");
   return (
     <div>
-      <div style={styleInput}>
-        <form onSubmit={handleAddData}>
-          <input placeholder="judul" onChange={handleChangeTitle} />
-          <input placeholder="isi" onChange={handleChangeContent} />
-          <button>Add</button>
-        </form>
-      </div>
-      <div style={styleListCard}>{ListCard}</div>
+      <form onSubmit={handleAddData}>
+        <input
+          placeholder="judul"
+          onChange={e => setTitleVals(e.target.value)}
+          value={titleVals}
+        />
+        <input
+          placeholder="isi"
+          onChange={e => setContentVals(e.target.value)}
+          value={contentVals}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 };
